8e941e4e13b740e0e5860dba29fa6e30790ab75a
Niccolò Toccane <nico.toccane@gmail.com>
2020-12-24 14:46:59 +0100

    connect to mongo-atlas DB

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ import indexRouter from './routes/index.js';
 
 dotenv.config();
 
+const { HOST, PORT, MONGO_USERNAME, MONGO_PASSWORD } = process.env;
+
 const app = express();
 
 app.use(logger('dev'));
@@ -18,18 +20,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/games', wordsRouter);
 app.use('/', indexRouter);
 
-mongoose.connect('mongodb://localhost:27017/hangman-backend', {
-	useNewUrlParser    : true,
-	useUnifiedTopology : true,
-	useCreateIndex     : true,
-	useFindAndModify   : false
-});
+mongoose.connect(
+	`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.vpqp4.mongodb.net/<dbname>?retryWrites=true&w=majority`,
+	{
+		useNewUrlParser    : true,
+		useUnifiedTopology : true,
+		useCreateIndex     : true,
+		useFindAndModify   : false
+	}
+);
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('successfully connected to the DB!'));
-
-const { HOST, PORT } = process.env;
 
 app.listen(PORT, HOST);
